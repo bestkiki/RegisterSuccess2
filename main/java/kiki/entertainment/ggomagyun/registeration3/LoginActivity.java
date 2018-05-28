a39f2f640801ac47d7e52e7ad63c866f208324fa
bestkiki <bestkiki3@gmail.com>
2018-05-28 21:48:30 +0900

    Add files via upload

---
+++ main/java/kiki/entertainment/ggomagyun/registeration3/LoginActivity.java
@@ -0,0 +1,35 @@
+package kiki.entertainment.ggomagyun.registeration3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        TextView registerButton = (TextView) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+    }
+}
---
+++ main/java/kiki/entertainment/ggomagyun/registeration3/RegisterRequest.java
@@ -0,0 +1,27 @@
+package kiki.entertainment.ggomagyun.registeration3;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+
+    final static private String URL = "http://businessgyun.dothome.co.kr/UserRegister.php";
+    private Map<String, String> parameters;
+
+    public RegisterRequest(String userID, String userPassword, String userName, int userAge, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+        parameters.put("userPassword", userPassword);
+        parameters.put("userName", userName);
+        parameters.put("userAge", userAge + "");
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return parameters;
+    }
+}
